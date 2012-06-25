f73a9463d855785ae7fb878009c66a43c0a29e25
Paul Dille <pdille@cmucreatelab.org>
2012-06-25 11:46:27 -0400

    Windows installer script now compiles tilestacktool before coping it over; Added path info to dll dependencies in final print message

--- tmc/make-win-installer.bat
+++ tmc/make-win-installer.bat
@@ -6,18 +6,23 @@ xcopy ..\ct\backports installer\ct\backports /E /C /R /I /K /Y
 xcopy ..\ct\json installer\ct\json /E /C /R /I /K /Y
 xcopy ..\ct\exifr installer\ct\exifr /E /C /R /I /K /Y
 for %%I in (..\ct\*.rb) do copy %%I installer\ct
+echo Making tilestacktool.exe
+cd ..\tilestacktool
+make
+cd ..\tmc
 xcopy ..\tilestacktool\tilestacktool.exe installer\tilestacktool\ /I
 xcopy ..\tilestacktool\ffmpeg\windows\ffmpeg.exe installer\tilestacktool\ffmpeg\windows\ /I
 xcopy ..\ruby installer\ruby /E /C /R /I /K /Y
 xcopy ..\time-machine-explorer installer\time-machine-explorer /E /C /R /I /K /Y
+echo Making tmc.exe
 nmake release
 copy release\tmc.exe installer
 echo.
 echo Now copy the following dll files to the main installer directory:
-echo   msvcp100.dll
-echo   msvcr100.dll
-echo   QtCore4.dll
-echo   QtGui4.dll
-echo   QtNetwork4.dll
-echo   QtWebKit4.dll
-echo   imageformats\qjpeg4.dll
+echo   Drive:\WINDOWS\system32\msvcp100.dll
+echo   Drive:\WINDOWS\system32\msvcr100.dll
+echo   Drive:\QtSDK\Desktop\Qt\4.8.x\bin\QtCore4.dll
+echo   Drive:\QtSDK\Desktop\Qt\4.8.x\bin\QtGui4.dll
+echo   Drive:\QtSDK\Desktop\Qt\4.8.x\bin\QtNetwork4.dll
+echo   Drive:\QtSDK\Desktop\Qt\4.8.x\bin\QtWebKit4.dll
+echo   Drive:\QtSDK\Desktop\Qt\4.8.x\plugins\imageformats\*.dll
