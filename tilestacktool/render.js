4f44451cd7ffe9a9e5bfa7100ff4dba07b8bdc81
Randy Sargent <randy.sargent@gmail.com>
2013-01-24 16:24:34 -0500

    Refactored FfmpegEncoder to separate files

--- tilestacktool/render.js
+++ tilestacktool/render.js
@@ -2,13 +2,16 @@ var system = require('system');
 var template_path = system.args[1];
 var frame = system.args[2];
 var destination = system.args[3];
+var width = system.args[4];
+var height = system.args[5];
 
 var page = require('webpage').create();
 var url = template_path + '#' + frame;
-console.log('Loading ' + url);
+page.viewportSize = {width:width, height:height};
+page.clipRect = {top:0, left:0, width:width, height:height};
+
 page.open(url, function (status) {
-    console.log('Loaded.');
-    console.log('Rendering to ' + destination);
+    console.log('Rendering ' + url + ' to ' + destination);
     page.render(destination);
     phantom.exit();
 });
--- tilestacktool/tilestacktool.cpp
+++ tilestacktool/tilestacktool.cpp
@@ -19,7 +19,6 @@
 #include "GPTileIdx.h"
 #include "ImageReader.h"
 #include "ImageWriter.h"
-#include "qt-faststart.h"
 
 #include "io.h"
 
@@ -28,6 +27,7 @@
 #include "Tilestack.h"
 #include "tilestacktool.h"
 #include "warp.h"
+#include "FfmpegEncoder.h"
 
 #define TODO(x) do { fprintf(stderr, "%s:%d: error: TODO %s\n", __FILE__, __LINE__, x); abort(); } while (0)
 
@@ -35,7 +35,6 @@ unsigned int tilesize = 512;
 bool create_parent_directories = false;
 bool delete_source_tiles = false;
 std::vector<std::string> source_tiles_to_delete;
-std::string ffmpeg_path_override;
 std::string render_js_path_override;
 
 void usage(const char *fmt, ...);
@@ -759,135 +758,6 @@ void save(std::string dest)
   fprintf(stderr, "Created %s\n", dest.c_str());
 }
 
-// OSX: Download ffmpeg binary from ffmpegmac.net
-class FfmpegEncoder {
-  int width, height;
-  FILE *out;
-  size_t total_written;
-  std::string tmp_filename;
-  std::string dest_filename;
-public:
-  
-  static bool test() {
-    std::string cmdline = string_printf("\"%s\" -loglevel error -version", path_to_ffmpeg().c_str());
-    FILE *ffmpeg = popen_utf8(cmdline.c_str(), "wb");
-    if (!ffmpeg) {
-      fprintf(stderr, "FfmpegEncoder::test: Can't run '%s'.  This is likely due to an installation problem.\n", cmdline.c_str());
-      return false;
-    }
-    while (fgetc(ffmpeg) != EOF) {}
-    int status = pclose(ffmpeg);
-    if (status) {
-      fprintf(stderr, "FfmpegEncoder::test:  pclose '%s' returns status %d.  This is likely due to an installation problem.\n", cmdline.c_str(), status);
-      return false;
-    }
-
-    fprintf(stderr, "ffmpeg: success\n");
-
-    // No longer using qt-faststart executable;  instead, using internal qt_faststart function    
-    // cmdline = string_printf("\"%s\"", path_to_qt_faststart().c_str());
-    // status = system(cmdline.c_str());
-    // if (status != 0) {
-    //   fprintf(stderr, "FfmpegEncoder::test: Can't run '%s'.  This is likely due to an installation problem.\n", cmdline.c_str());
-    //   return false;
-    // }
-    // fprintf(stderr, "qt-faststart: success\n");
-
-    return true;
-  }
-  
-  FfmpegEncoder(std::string dest_filename, int width, int height,
-                double fps, double compression) :
-    width(width), height(height), total_written(0), dest_filename(dest_filename) {
-    tmp_filename = temporary_path(filename_sans_directory(dest_filename));
-    int nthreads = 8;
-    std::string cmdline = string_printf("\"%s\" -threads %d -loglevel error -benchmark", path_to_ffmpeg().c_str(), nthreads);
-    // Input
-    cmdline += string_printf(" -s %dx%d -vcodec rawvideo -f rawvideo -pix_fmt rgb24 -r %g -i pipe:0",
-                             width, height, fps);
-    // Output
-    int frames_per_keyframe = 10; // TODO(rsargent): don't hardcode this
-    cmdline += " -vcodec libx264";
-    //cmdline += string_printf(" -fpre libx264-hq.ffpreset");
-    cmdline += " -coder 1 -flags +loop -cmp +chroma -partitions +parti8x8+parti4x4+partp8x8+partb8x8 -me_method umh -subq 8 -me_range 16 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -b_strategy 2 -qcomp 0.6 -qmin 10 -qmax 51 -qdiff 4 -refs 4 -directpred 3 -trellis 1 -flags2 +wpred+mixed_refs+dct8x8+fastpskip";
-
-    cmdline += string_printf(" -crf %g -g %d -bf 0 \"%s\"",
-                             compression, frames_per_keyframe, tmp_filename.c_str());
-    fprintf(stderr, "Cmdline: %s\n", cmdline.c_str());
-
-    #ifdef _WIN32
-      putenv("AV_LOG_FORCE_NOCOLOR=1");
-    #else
-      setenv("AV_LOG_FORCE_NOCOLOR", "1", 1);
-    #endif
-
-    unlink(dest_filename.c_str());
-    out = popen_utf8(cmdline, "wb");
-    if (!out) {
-      throw_error("Error trying to run ffmpeg.  Make sure it's installed and in your path\n"
-                  "Tried with this commandline:\n"
-                  "%s\n", cmdline.c_str());
-    }
-  }
-
-  void write_pixels(unsigned char *pixels, size_t len) {
-    //fprintf(stderr, "Writing %zd bytes to ffmpeg\n", len);
-    if (1 != fwrite(pixels, len, 1, out)) {
-      throw_error("Error writing to ffmpeg");
-    }
-    total_written += len;
-  }
-
-  void close() {
-    if (out) pclose(out);
-    fprintf(stderr, "Wrote %ld frames (%ld bytes) to ffmpeg\n", 
-            (long) (total_written / (width * height * 3)), (long) total_written);
-    out = NULL;
-    //std::string cmd = string_printf("\"%s\" \"%s\" \"%s\"", path_to_qt_faststart().c_str(), tmp_filename.c_str(), dest_filename.c_str());
-    //if (system_utf8(cmd)) {
-    //  throw_error("Error running qtfaststart: '%s'", cmd.c_str());
-    //}
-    qt_faststart(tmp_filename, dest_filename);
-    delete_file(tmp_filename);
-  }
-
-  
-  static std::string path_to_ffmpeg() {
-    if (ffmpeg_path_override != "") return ffmpeg_path_override; 
-    return path_to_executable("ffmpeg");
-  }
-
-  static std::string path_to_qt_faststart() {
-    return path_to_executable("qt-faststart");
-  }
-    
-  static std::string path_to_executable(std::string executable) {
-    executable += executable_suffix();
-    std::vector<std::string> search_path;
-    search_path.push_back(string_printf("%s/ffmpeg/%s/%s",
-					filename_directory(executable_path()).c_str(),
-					os().c_str(),
-					executable.c_str()));
-    search_path.push_back(string_printf("%s/%s",
-					filename_directory(executable_path()).c_str(),
-					executable.c_str()));
-    
-    for (unsigned i = 0; i < search_path.size(); i++) {
-      if (filename_exists(search_path[i])) {
-	fprintf(stderr, "Found %s at path %s\n", executable.c_str(), search_path[i].c_str());
-	return search_path[i];
-      }
-    }
-    
-    fprintf(stderr, "Could not find %s in search path:\n", executable.c_str());
-    for (unsigned i = 0; i < search_path.size(); i++) {
-      fprintf(stderr, "%s\n", search_path[i].c_str());
-    }
-    fprintf(stderr, "Using version in PATH, if available\n");
-    return executable;
-  }
-};
-
 class PrependLeaderTilestack : public LRUTilestack {
   simple_shared_ptr<Tilestack> source;
   unsigned leader_nframes;
@@ -1436,11 +1306,13 @@ public:
     //  maybe pass floating point to html page to render an animation between labels?
     int source_frameno = (int) frames[frame].frameno;
     
-    std::string cmd = string_printf("phantomjs %s %s %d %s",
+    std::string cmd = string_printf("phantomjs %s %s %d %s %d %d",
                                     path_to_render_js.c_str(),
                                     overlay_html_path.c_str(),
                                     source_frameno,
-                                    overlay_image_path.c_str());
+                                    overlay_image_path.c_str(),
+                                    tile_width,
+                                    tile_height);
     if (system(cmd.c_str())) {
       throw_error("Error executing '%s'", cmd.c_str());
     }
@@ -1483,11 +1355,11 @@ void usage(const char *fmt, ...) {
           "--path2stack width height [frame1, ... frameN] stackset\n"
           "        Frame format {\"frameno\":N, \"bounds\": {\"xmin\":N, \"ymin\":N, \"xmax\":N, \"ymax\":N}\n"
           "        Multiframe with single bounds. from and to are both inclusive.  step defaults to 1:\n"
-          "          {\"frames\":{\"from\":N, \"to\":N, \"step\":N], \"bounds\": {\"xmin\":N, \"ymin\":N, \"xmax\":N, \"ymax\":N}\n"
+          "          {\"frames\":{\"from\":N, \"to\":N, \"step\":N}, \"bounds\": {\"xmin\":N, \"ymin\":N, \"xmax\":N, \"ymax\":N}\n"
           "--path2stack-from-stack width height [frame1, ... frameN]\n"
           "        Frame format {\"frameno\":N, \"bounds\": {\"xmin\":N, \"ymin\":N, \"xmax\":N, \"ymax\":N}\n"
           "        Multiframe with single bounds. from and to are both inclusive.  step defaults to 1:\n"
-          "          {\"frames\":{\"from\":N, \"to\":N, \"step\":N], \"bounds\": {\"xmin\":N, \"ymin\":N, \"xmax\":N, \"ymax\":N}\n"
+          "          {\"frames\":{\"from\":N, \"to\":N, \"step\":N}, \"bounds\": {\"xmin\":N, \"ymin\":N, \"xmax\":N, \"ymax\":N}\n"
           "--path2overlay width height [frame1, ... frameN] overlay.html\n"
           "        Create tilestack by rendering overlay.html#FRAMENO for each frame in path.  Leave background in overlay.html unset\n"
           "        to create an overlay with transparent background\n"
@@ -1695,7 +1567,7 @@ int main(int argc, char **argv)
         create_parent_directories = true;
       }
       else if (arg == "--ffmpeg-path") {
-        ffmpeg_path_override = args.shift();
+        FfmpegEncoder::ffmpeg_path_override = args.shift();
       }
       else if (arg == "--render-path") {
         render_js_path_override = args.shift();
