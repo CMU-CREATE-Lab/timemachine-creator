bb08d275f98d08e9d203ee7de3e1f865115d920b
Paul Dille <pdille@cmucreatelab.org>
2014-12-16 13:04:02 -0500

    - Added missing file from backports commit

--- ct/backports.rb
+++ ct/backports.rb
@@ -1,3 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/backports/tools")
-Backports.require_relative "backports/1.9"
-Backports.require_relative "backports/rails"
+require "backports/tools"
+require "backports/version"
+require "backports/2.1"
+require "backports/rails"
